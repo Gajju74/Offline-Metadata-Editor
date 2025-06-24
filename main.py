f35e84aaf4d68f418d61a639f15b3f721db85b9b
Ashwin Himanshu <ashwinhimanshu11@gmail.com>
2025-06-24 17:55:11 +0530

    added noise removal

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 # main.py
-import sys
+# import sys
+# from PySide6.QtWidgets import QApplication
+# from ui.file_browser import FileBrowser
+
+# if __name__ == "__main__":
+#     app = QApplication(sys.argv)
+#     window = FileBrowser()
+#     window.show()
+#     sys.exit(app.exec())
+
 from PySide6.QtWidgets import QApplication
 from ui.dashboard import MainWindow  # new dashboard file
 
